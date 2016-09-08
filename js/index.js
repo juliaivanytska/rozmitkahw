372f5059744e272d38aac54d9e50beac937aba91
Yuliya <juliaivanytska15@gmail.com>
2016-09-08 10:55:40 +0000

    new

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-function clock(){
-   $('body').html((new Date).toLocaleTimeString())
- }
+$ (function () {
+    $('#knopka').click(sendForm);
+   
+});
+    function sendForm(e) {e.preventDefault();
+       $.ajax({
+          url:"https://formspree.io/juliaivanytska15@gmail.com",
+          method:"POST",
+          data:{
+             marka:$('#marka').val(),
+             model:$('#model').val(),
+             year:$('#year').val(),
+             v3:$('#v3').val(),
+             korobka:$('#korobka').val(),
+             user_name:$('#user_name').val(),
+             emaill:$('#emaill').val(),
+             phone_number:$('#phone_number').val(),
+             addwish:$('#addwish').val()
+          },
+          dataType:'json',
+          success:function() {
+             $('#thanks').html('Thank you for contacting us!');
+          }
+       });
+    };
